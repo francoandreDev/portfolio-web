d614b74c589f60232218eb7f758cd34210f3bf01
francoandre.dev <francoandre.dev@gmail.com>
2024-01-17 19:51:52 -0500

    feat(brand component): end the main idea for the brand, the logo is pending yet

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -7,8 +7,12 @@ import { Letter } from './exports.ts';
 
 export default component$(({ id }: { id: unknown }) => {
     return (
-        <span class={`${brandStyles.inline}`}>
-            {nameBrand.split("").map((letter, index) => <Letter letter={letter} key={"" + id + index + letter} />)}
+        <span class={`${brandStyles.inline} ${brandStyles.selection}`}>
+            {nameBrand.split("").map((letter: string, index: number) => {
+                const timeAppear = index / 3
+                const keyLetter = "" + id + index + letter
+                return (<Letter letter={letter} sTime={timeAppear} key={keyLetter} />);
+            })}
         </span>
     );
 });
--- src/components/brand/Letter.tsx
+++ src/components/brand/Letter.tsx
@@ -1,9 +1,40 @@
-import { component$ } from '@builder.io/qwik';
+import { $, component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 
 import brandStyles from "../../modules/brand.module.css"
 
-export default component$(({ letter }: { letter: string }) => {
+const colorsList: string[] = ["red", "purple", "green", "gold", "blue"]
+const seconds: number = 1000
+
+export default component$(({ letter, sTime }: { letter: string, sTime: number }) => {
+    const indexColors = useSignal<number>(0);
+    const classColor = useSignal<string>(brandStyles["letter-red"])
+    const classVisible = useSignal<string>(brandStyles["hidden"])
+    const totalReps: number = 4
+
+    const beVisible = $(() => {
+        classVisible.value = brandStyles["show"]
+    });
+
+    const changeColor = $(() => {
+        indexColors.value = (indexColors.value + 1) % colorsList.length
+        classColor.value = brandStyles[`letter-${colorsList[indexColors.value]}`]
+    });
+
+    // eslint-disable-next-line qwik/no-use-visible-task
+    useVisibleTask$(async () => {
+        setTimeout(() => {
+            beVisible()
+            let rep = 0
+            const interval = setInterval(() => {
+                changeColor()
+                rep++
+                if (rep > totalReps) clearInterval(interval)
+            }, 2 * seconds)
+        }, sTime * seconds)
+    });
+
+
     return (
-        <div class={`${brandStyles.letter} ${brandStyles["brand-font"]}`}>{letter}</div>
+        <div class={`${brandStyles.letter} ${brandStyles["brand-font"]} ${classVisible.value} ${classColor.value}`}>{letter}</div>
     );
 });
